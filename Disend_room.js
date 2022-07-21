b2630fd5c76881cd7066c0534de0910eb1f54552
YD-0110 <90946473+YD-0110@users.noreply.github.com>
2022-07-21 18:26:38 +1000

    Add files via upload

---
+++ Disend_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC4e7CQbnRNecOZlj-zJZ657Dk6jZQEQNM",
+    authDomain: "disend-7569b.firebaseapp.com",
+    databaseURL:"https://disend-7569b-default-rtdb.firebaseio.com",
+    projectId: "disend-7569b",
+    storageBucket: "disend-7569b.appspot.com",
+    messagingSenderId: "1023230371228",
+    appId: "1:1023230371228:web:fd364a6d8d374c7f9ed459"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
